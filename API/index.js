8f2ec756ceb721f55a0ac407de8288cd24108cf1
Onwuka Chisom Stanley <43853290+StanleyTee@users.noreply.github.com>
2019-02-18 05:02:38 +0100

    Installed bodyParser

--- API/index.js
+++ API/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+import bodyParser from 'body-parser';
 
 const app = express();
 
 const PORT = 8080;
 
+app.use(bodyParser.json());
 app.get('/', function(req,res){
     return res.send('The API is working');
 });
