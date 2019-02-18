c2ca3cb3ba3d9009a6b0902667df81cf6bae0f94
StanleyTee <kachi2505@yahoo.com>
2019-02-18 04:53:50 +0100

    create controllers

---
+++ API/controllers/meal.controller.js
@@ -0,0 +1,35 @@
+import MealService from '../services/meal.service';
+
+const MealController = {
+    fetchAllMeals(req,res) {
+        return res.json({
+            status:'success',
+            data:allMeals
+        }).status(200);
+    },
+    addAMeal(req,res) {
+        /*
+        json format
+        {
+            name:"food-name",
+            plate:1
+            "price":500
+        }
+        */
+        const newMeal = req.body;
+        return createdMeal = MealService.addMeal(newMeal);
+        return res.json({
+            status:'success',
+            data: createdMeal
+        }).status(201);
+    },
+    getSingleMeal(req,res){
+        const id = req.params.id;
+        const foundMeal = MealService.getAMeal(id);
+        return res.json({
+            status:'success',
+            data: createdMeal
+        }).status(201);
+    }
+}
+
